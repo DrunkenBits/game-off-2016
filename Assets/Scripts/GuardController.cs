4794963aa5c6a114f1b5fdf653714a84376fc954
Luca Barone <baro.luc@gmail.com>
2016-11-13 18:02:35 +0100

    Adds basic animation controller for the guard

--- Assets/Scripts/GuardController.cs
+++ Assets/Scripts/GuardController.cs
@@ -15,48 +15,68 @@ public class GuardController : Glitchable {
   public List<GuardPatrolStep> sequence = new List<GuardPatrolStep>();
   public float speed = 3.5f;
   public float minDistance = 0.01f;
+  public float minPlayerDistance = 30.0f;
 
   protected int nextPoint = 0;
   protected Rigidbody2D rb;
+  protected Animator anim;
   protected Vector3 initialScale = new Vector3(1, 1, 1);
   protected float? waitStartTime = null;
   protected float? waitTime = null;
 
+  protected string state = "patroling";
+
   public void Start() {
     this.rb = this.GetComponent<Rigidbody2D>();
     this.initialScale = this.transform.localScale;
+    this.anim = this.GetComponent<Animator>();
   }
 
   public void FixedUpdate() {
-    if (this.waitStartTime.HasValue && this.waitTime.HasValue) {
-      if ((Time.time - this.waitStartTime.GetValueOrDefault()) * this.getTime() < this.waitTime.GetValueOrDefault()) {
-        return;
+    GameObject player = GameObject.FindWithTag("Player");
+
+    if (this.state == "patroling") {
+      if (player) {
+        float playerDist = player.transform.position.x - this.transform.position.x;
+
+        if (Mathf.Abs(playerDist) < this.minPlayerDistance) {
+          if (Mathf.Sign(playerDist) == Mathf.Sign(this.transform.localScale.x)) {
+            this.anim.SetTrigger("Fire");
+            this.state = "attacking";
+          }
+        }
       }
 
-      this.waitStartTime = null;
-      this.waitTime = null;
-    }
+      if (this.waitStartTime.HasValue && this.waitTime.HasValue) {
+        if ((Time.time - this.waitStartTime.GetValueOrDefault()) * this.getTime() < this.waitTime.GetValueOrDefault()) {
+          return;
+        }
 
-    GuardPatrolStep step = this.sequence[this.nextPoint % this.sequence.Count];
-    Vector2 nextPointVec = this.points[step.index];
-    float dist = nextPointVec.x - this.transform.position.x;
-
-    if (Mathf.Abs(dist) < this.minDistance) {
-      nextPoint = (nextPoint + 1) % this.sequence.Count;
-
-      if (step.canWait) {
-        float amount = Random.Range(step.minWaitTime, step.maxWaitTime);
-        Debug.Log("Waiting");
-        Debug.Log(amount);
-
-        this.waitStartTime = Time.time;
-        this.waitTime = amount;
+        this.waitStartTime = null;
+        this.waitTime = null;
       }
 
-    } else {
-      bool right = this.transform.position.x < nextPointVec.x;
-      this.transform.localScale = new Vector3(initialScale.x * (right ? 1 : -1), initialScale.y, initialScale.z);
-      this.rb.velocity = new Vector2(this.speed * (right ? 1 : -1) * this.getTime(), this.rb.velocity.y);
+      GuardPatrolStep step = this.sequence[this.nextPoint % this.sequence.Count];
+      Vector2 nextPointVec = this.points[step.index];
+      float dist = nextPointVec.x - this.transform.position.x;
+
+      if (Mathf.Abs(dist) < this.minDistance) {
+        nextPoint = (nextPoint + 1) % this.sequence.Count;
+
+        if (step.canWait) {
+          float amount = Random.Range(step.minWaitTime, step.maxWaitTime);
+          Debug.Log("Waiting");
+          Debug.Log(amount);
+
+          this.waitStartTime = Time.time;
+          this.waitTime = amount;
+        }
+
+      } else {
+        bool right = this.transform.position.x < nextPointVec.x;
+        this.transform.localScale = new Vector3(initialScale.x * (right ? 1 : -1), initialScale.y, initialScale.z);
+        this.rb.velocity = new Vector2(this.speed * (right ? 1 : -1) * this.getTime(), this.rb.velocity.y);
+      }
     }
   }
 
